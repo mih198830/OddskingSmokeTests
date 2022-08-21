937ba366eacc69dc7d39184be64a5d66a83b867d
mikhail.matskevich <mih198830@gmail.com>
2022-08-21 18:27:52 +0400

    pages added

--- src/test/java/cloud/autotests/tests/TestBase.java
+++ src/test/java/cloud/autotests/tests/TestBase.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 @ExtendWith({AllureJunit5.class})
 public class TestBase {
     String siteUrl = "https://www.oddsking.com/";
+
     @BeforeAll
     static void beforeAll() {
         DriverSettings.configure();
