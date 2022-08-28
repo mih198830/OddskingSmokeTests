030656b4558d4613f09e3b2f5d95eb71558caaed
mikhail.matskevich <mih198830@gmail.com>
2022-08-28 21:18:54 +0400

    images added

--- src/test/java/com/sharpgaming/tests/SmokeSharpgamingTests.java
+++ src/test/java/com/sharpgaming/tests/SmokeSharpgamingTests.java
@@ -1,15 +1,11 @@
 package com.sharpgaming.tests;
 
-import com.codeborne.selenide.Condition;
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byValue;
-import static com.codeborne.selenide.Selectors.withTagAndText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.sleep;
 import static io.qameta.allure.Allure.step;
 
 
