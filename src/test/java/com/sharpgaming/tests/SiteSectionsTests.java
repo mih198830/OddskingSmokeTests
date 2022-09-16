276e52785328928d2f4df6b944630b806070052f
mikhail.matskevich <mih198830@gmail.com>
2022-09-16 12:26:01 +0400

    main class renamed, allure steps refactored

--- src/test/java/com/sharpgaming/tests/SiteSectionsTests.java
+++ src/test/java/com/sharpgaming/tests/SiteSectionsTests.java
@@ -8,28 +8,28 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
-public class SmokeSharpgamingTests extends TestBase {
+public class SiteSectionsTests extends TestBase {
     @Test
     @Description("Check that email link exist on the About Us page")
     @DisplayName("Email link test - About us page ")
     void emailLinkTest() {
 
-        step("Click About Us Link", () -> {
-            homePage.aboutUsLinkClick();
-        });
+        step("Click About Us Link", () ->
+            homePage.aboutUsLinkClick()
+        );
 
-        step("Check email address is present on the About Us page", () -> {
-            aboutUsPage.emailAddress();
-        });
+        step("Check email address is present on the About Us page", () ->
+            aboutUsPage.emailAddress()
+        );
     }
 
     @Test
     @Description("Open What we Do page and click Vacancies button")
     @DisplayName("Open What we Do page and open Vacancies")
     void checkVacanciesFromWhatWePage() {
-        step("Click What we do Link", () -> {
-            homePage.whatWeDoLinkClick();
-        });
+        step("Click What we do Link", () ->
+            homePage.whatWeDoLinkClick()
+        );
 
         step("Click Vacancies from What we do page", () -> {
             whatWeDoPage.vacanciesButtonClick();
@@ -41,13 +41,13 @@ public class SmokeSharpgamingTests extends TestBase {
     @Description("Open Vacancies page and check vacancies block is available")
     @DisplayName("Vacancies block available for users")
     void vacanciesBlockAvailable() {
-        step("Click What we do Link", () -> {
-            homePage.whatWeDoLinkClick();
-        });
+        step("Click What we do Link", () ->
+            homePage.whatWeDoLinkClick()
+        );
 
-        step("Check vacancies block is visible on the Vacancies page", () -> {
-            whatWeDoPage.vacanciesButtonClick();
-        });
+        step("Check vacancies block is visible on the Vacancies page", () ->
+            whatWeDoPage.vacanciesButtonClick()
+        );
     }
 
     @Test
