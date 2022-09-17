b088870fc19d4c7471cc1a7df05367d0663e0c89
mikhail.matskevich <mih198830@gmail.com>
2022-09-17 11:34:14 +0400

    exist assert removed

--- src/test/java/com/sharpgaming/tests/pages/AboutUsPage.java
+++ src/test/java/com/sharpgaming/tests/pages/AboutUsPage.java
@@ -3,13 +3,17 @@ package com.sharpgaming.tests.pages;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.href;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
 
 public class AboutUsPage {
-    SelenideElement emailAddress = $(By.linkText("info@sharpgaming.com"));
+
+    SelenideElement emailAddress = $(By.partialLinkText("info@sharpgaming.com"));
 
     public AboutUsPage emailAddress() {
-        emailAddress.exists();
+        executeJavaScript("window.scrollTo(0, document.body.scrollHeight)");
+        emailAddress.shouldHave(href("mailto:info@sharpgaming.com"));
         return this;
     }
 }
